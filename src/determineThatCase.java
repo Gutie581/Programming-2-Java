ef54138da0c82c1bf5183694273e16429a181514
Gutie581 <Charlee581!QAZ@WSX>
2024-09-10 13:30:05 -0500

    Commit test 1

---
+++ src/determineThatCase.java
@@ -0,0 +1,26 @@
+/*
+
+                    Determine That Case!
+
+Your Java program will perform the following tasks:
+
+1. Using Java Swing, determine whether a character entered by the user is uppercase or lowercase.
+
+2. Inform the user of the result.
+
+3. IF the user enters a value other than an alphabetical letter ( A-Z, a-z), tell the user that you cannot determine the case of a value other than an alphabetical character.
+
+You do not need another method here, just use the main method
+
+ */
+
+public class determineThatCase
+{
+    public static void main(String[] args) // Define main method
+    {
+
+        System.out.println("Hello world!");
+
+
+    }
+}
