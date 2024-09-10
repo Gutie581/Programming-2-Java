12b7ebfcd2babc6bf6b9d524ba3ba340455541ed
Gutie581 <Charlee581!QAZ@WSX>
2024-09-10 13:27:01 -0500

    Commit test 1

---
+++ src/magicDates.java
@@ -0,0 +1,40 @@
+// Challenge 02 - Daniel Gutierrez - KEA418
+
+// import required for JOptionPane
+import javax.swing.JOptionPane;
+
+public class magicDates
+{
+    public static void main(String[] args) // Define main method
+    {
+        // User input for month.
+        String monthDate = JOptionPane.showInputDialog("Please enter a month in numeric format: ");
+        int month = Integer.parseInt(monthDate);         // Converts string to integer.
+
+        // User input for day.
+        String dayDate = JOptionPane.showInputDialog("Please enter a day: ");
+        int day = Integer.parseInt(dayDate);            // Converts string to integer.
+
+        // User input for year
+        String yearDate = JOptionPane.showInputDialog("Please enter a year: ");
+        int year = Integer.parseInt(yearDate);              // Converts string to integer.
+
+        // Displays results in an if else statement.
+        String message;
+        if (month * day == year)            // Multiples month and day then compares it equal to the year.
+        {
+            message = "The date is a magic date! Wow!";
+        }
+        else
+        {
+            message = "The date is not a magic date.";
+        }
+
+        // Displays the results
+        JOptionPane.showMessageDialog(null, message);
+
+        // Ends the program
+        System.exit(0);
+    }
+
+}
