af4a5cd498533d81f3534f235f0d0cc11f0c452a
Gutie581 <daniel.gutierrez4@my.utsa.edu>
2024-09-18 19:08:02 -0500

    Class notes for 9/18/24

---
+++ src/Notes9_16_24.java
@@ -0,0 +1,57 @@
+
+
+//public class Notes9_16_24 {}
+
+/*
+
+Opening and Inputting Files Input/Output (I/O)
+
+import javax.swing.*;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.FileReader;
+
+for (int i = 0; i < 5; i++)
+standard syntax: for (initialization; condition; increment/decrement)
+(start; where to go; how much to increment)
+" i is iterated"
+++ increments last after the code executes. -- increments first.
+
+
+Sentinel Value
+
+
+PrintWriter & FileWriter
+Create a new file using PrintWriter (puts data in there) and FileWriter (Creates file).
+Use try catch block. (Try, Except in Python)
+It is a MUST in order for program not to crash on us.
+Module 4 code (Lines 133 - 154)
+
+****************** REMEMBER THIS FORMULA!!!!
+Class Variable = new SameClass
+FileWriter fileWriter = new FileWriter("/Users/burres/Desktop/output.txt");
+Class VariableName = new "Object of the FileWriter Class" (Here is my reference point);
+FileWriter variable just points to the object stored in memory.
+The output.txt is an object of the FileWriter Class.
+Use my own path to test it but submit to that path mentioned above. (/Users/burres/Desktop/output.txt
+Line 151 - Generalized exception to catch anything. Catch e "everything".
+
+Open a file using Scanner and FileReader
+Module 4 (Lines 156 - 166)
+
+Open a file using a GUI, write to file, open file and contents using GUI
+Module 4 (Lines 177 - 187)
+
+
+
+
+
+Later will review: Soft copy or Hard copy
+
+
+
+
+
+
+
+*/
